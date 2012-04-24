bc168be27f7b1adba6adf7494f1f39c6ce7038a7
Robert Wetzlmayr <robert@wetzlmayr.at>
2012-04-24 08:54:24 +0200

    Formatting.

--- wet_sample_js_i18n.php
+++ wet_sample_js_i18n.php
@@ -3,38 +3,38 @@ class wet_sample_js_i18n
 {
 	function __construct()
 	{
-        register_callback(array(__CLASS__ , 'add_i18n_entries'), 'admin_side', 'head_end');
+		register_callback(array(__CLASS__ , 'add_i18n_entries'), 'admin_side', 'head_end');
 	}
 
-    function add_i18n_entries()
-    {
-        // Which strings do we need to push to the client?
-        $keys = array(
-            'save',
-            'publish',
-            'category1'
-        );
+	function add_i18n_entries()
+	{
+		// Which strings do we need to push to the client?
+		$keys = array(
+			'save',
+			'publish',
+			'category1'
+		);
 
-        // Render as a JS collection
-        foreach($keys as $key) {
-            $out[] = "$key: \"".gTxt($key).'"';
-        }
+		// Render as a JS collection
+		foreach($keys as $key) {
+			$out[] = "$key: \"".gTxt($key).'"';
+		}
 
-        // Add a manual entry because we can
-        $out[] = 'wet_foo: "Foo!"';
+		// Add a manual entry because we can
+		$out[] = 'wet_foo: "Foo!"';
 
-        $out = join(', '.n, $out);
+		$out = join(', '.n, $out);
 
-        // Push to client
-        $js = <<<EOS
-            $(document).ready(function() {
-                $.extend(textpattern.textarray, {
-                {$out}
-                });
-            });
+		// Push to client
+		$js = <<<EOS
+			$(document).ready(function() {
+				$.extend(textpattern.textarray, {
+				{$out}
+				});
+			});
 EOS;
-        echo script_js($js);
-    }
+		echo script_js($js);
+	}
 }
 
 if (txpinterface == 'admin') new wet_sample_js_i18n;
